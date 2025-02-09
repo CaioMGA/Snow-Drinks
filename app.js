1973bab596bbcaa0bd8bcfb7d247d987036e2957
Caio MGA <caiomga@gmail.com>
2025-02-09 10:58:49 -0300

    add ingredient selection; add music; add win conditions;

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 /**
  * S N O W D R I N K S
  */
+var pan = [] // brewing will take place here
+const ingredients = [
+    {id:0, name:"honey"},
+    {id:0, name:"garlic"},
+    {id:0, name:"fire"},
+    {id:0, name:"water"},
+    {id:0, name:"herbs"},
+    {id:0, name:"rum"},
+    {id:0, name:"cinnamon"},
+]
+
+var curLevel = 1
+var bgm
+var log = document.getElementById("feedback-log")
+
 
 const init = () => {
     // setup back buttons
@@ -24,11 +39,49 @@ const init = () => {
      * Setup Level Select
      */
 
-    setupButton("#select-lvl1", () => showScreen("#lvl1"))
-    setupButton("#select-lvl2", () => showScreen("#lvl2"))
-    setupButton("#select-lvl3", () => showScreen("#lvl3"))
+    setupButton("#select-lvl1", () => {
+        showScreen("#lvl1")
+        playLevel(1)
+    })
+    setupButton("#select-lvl2", () => {
+        showScreen("#lvl2")
+        playLevel(2)
+    })
+    setupButton("#select-lvl3", () => {
+        showScreen("#lvl3")
+        playLevel(3)
+    })
+
+    /* Setup Settings Screen*/
+    
+    document.getElementById("music-toggle").addEventListener("click", onMusicToggle)
 
 
+    /* Setup Gameplay buttons*/
+
+    for(let i = 0; i<ingredients.length; i++){
+        const btns = document.querySelectorAll(".btn"+i)
+
+        for(let btn of btns){
+            btn.addEventListener("click", () => {
+                addIngredientToPan(i)
+                btn.disabled = true
+                
+            })
+        }
+    }
+    
+    const restartButtons = document.querySelectorAll(".restart-level")
+
+    for(rb of restartButtons){
+        rb.addEventListener("click", () => {
+            playLevel(curLevel)
+        })
+    }
+
+
+    // music
+    playMusic()
 }
 
 const setupButton = (id, callback) => {
@@ -43,7 +96,129 @@ const showScreen = (id) => {
     }
 
     document.querySelector(id).classList.remove("hidden")
+    log.innerText = ""
 }
+
+playLevel = (levelNum) => {
+    const ingBtns = document.querySelectorAll(".ingredient-button")
+
+    for(btn of ingBtns){
+        btn.disabled = false
+    }
+    log.innerText = ""
+
+    pan = []
+    curLevel = levelNum
+    console.log("play cur level as ", curLevel)
+}
+
+const addIngredientToPan = (ingredientId) =>{
+    for(key of Object.keys(pan)){
+        pan[key]++
+    }
+
+    pan[ingredients[ingredientId].name] = 0
+    showIngredientLog(ingredients[ingredientId].name)
+    checkBrew()
+}
+
+const showIngredientLog = (ingredient) => {
+    if(ingredient == 'fire'){
+        log.innerText += "\nTurned fire on"
+    } else {
+        log.innerText += "\nAdded " + ingredient
+    }
+}
+
+const logThis = (msg) => {
+    log.innerText += "\n\n" + msg
+}
+const checkBrew = () => {
+
+    console.log(Object.keys(pan).length)
+    console.log(getCurLevelIngredientCount())
+    if(Object.keys(pan).length >= getCurLevelIngredientCount()){
+        evaluateBrew();
+    }
+}
+const evaluateBrew = () => {
+    var review = "\n\n=== BREW STATUS ===\n\n"
+    var issues = false
+    
+    if(pan["honey"] > pan["fire"] && pan["water"] < pan["fire"]){
+        review += "Honey got burned\n"
+        issues = true
+    }
+
+    if(pan["fire"] > pan["garlic"] && (pan["garlic"] - pan["water"]) < 2){
+        review += "Garlic got burned\n"
+        issues = true
+    }
+
+    if(pan["fire"] > pan["garlic"] && (pan["garlic"] - pan["water"]) > 2){
+        review += "Garlic got undercooked\n"
+        issues = true
+    }
+
+    if(pan["rum"] > 2){
+        review += "Rum evaporated\n"
+        issues = true
+    }
+
+    if(pan["fire"] == 0){
+        review += "It did not cook properly"
+        issues = true
+    }
+
+    if(pan["cinnamon"] > 0){
+        review += "Cinnamon lost its flavor"
+        issues = true
+    }
+
+    if(!issues){
+        review += "Perfect brew!"
+    }
+
+    logThis(review)
+}
+
+const playMusic = () => {
+    if (bgm){
+        bgm.pause()
+    }
+    bgm = new Audio('gameplay.ogg')
+    bgm.play()
+}
+
+const stopMusic = () => {
+    bgm.pause()
+}
+
+const onMusicToggle = () => {
+    console.log("on music toggle")
+    const tgl = document.getElementById("music-toggle")
+
+    if(tgl.checked){
+        playMusic()
+    } else {
+        stopMusic()
+    }
+
+}
+
+const getCurLevelIngredientCount = () => {
+    switch(curLevel){
+        case 1:
+            return 4
+        case 2:
+            return 5
+        case 3:
+            return 7
+        default:
+            return -1
+    }
+}
+
 
 
 init();
